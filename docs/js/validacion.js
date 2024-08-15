307983526005592bec7b20282e5143d495038d43
Walki17 <walkii_@hotmail.com>
2024-08-15 20:26:55 -0300

    Co-authored-by: CamilaScu <CamilaScu@users.noreply.github.com> Co-authored-by: aguslelop <aguslelop@users.noreply.github.com> Co-authored-by: TaysaE <TaysaE@users.noreply.github.com> Co-authored-by: Raisa Figueiredo <raoliverf@users.noreply.github.com>

--- docs/js/validacion.js
+++ docs/js/validacion.js
@@ -5,3 +5,39 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+let boton = document.getElementById("regBtn"); 
+let nombre = document.getElementById("nombre");
+let apellido = document.getElementById("apellido");
+let email = document.getElementById("email"); 
+let contrasenaUno = document.getElementById("password1");
+let contrasenaDos = document.getElementById("password2");
+let terminos = document.getElementById("terminos");
+
+
+
+
+boton.addEventListener('click', function() {
+    if (nombre.value === "" || apellido.value === "" || email.value === "" || contrasenaUno.value === "" || contrasenaDos.value === "") {
+        
+        return showAlertError();
+    }
+  else if (contrasenaUno.value.length < 6) {
+        
+        return showAlertError();
+    }
+  else if (contrasenaUno.value !== contrasenaDos.value) {
+       
+        return showAlertError();
+    }
+  else if (!terminos.checked) {
+        
+        return showAlertError();
+    }
+    else { 
+        return showAlertSuccess();
+   }
+
+  
+});
+
